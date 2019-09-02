818b7957a502d9932300d4ef1860155bef500fc5
yamashush <38120991+yamashush@users.noreply.github.com>
2019-09-02 14:23:33 +0900

    :fish: add plugin peco

--- .config/fish/conf.d/config.fish
+++ .config/fish/conf.d/config.fish
@@ -8,3 +8,10 @@ set -x PATH $HOME/.rbenv/bin $PATH
 # prompt
 #
 set -g fish_prompt_pwd_dir_length 0
+
+#
+# plugin-peco https://github.com/oh-my-fish/plugin-peco
+#
+function fish_user_key_bindings
+  bind \cr 'peco_select_history (commandline -b)'
+end
