404c4672a2df7b7301feb4e8eca2bbd67b1a5820
yamashush <38120991+yamashush@users.noreply.github.com>
2019-09-02 14:39:54 +0900

    :fish: add auto ls at cd

--- .config/fish/conf.d/config.fish
+++ .config/fish/conf.d/config.fish
@@ -15,3 +15,15 @@ set -g fish_prompt_pwd_dir_length 0
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
 end
+
+
+#
+# auto ls
+#
+function cd
+    if count $argv > /dev/null
+        builtin cd "$argv"; and ls
+    else
+        builtin cd ~; and ls
+    end
+end
