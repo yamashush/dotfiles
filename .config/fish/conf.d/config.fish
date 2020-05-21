208570562ce06a5e5fd660cd29adc83423c4c1dd
yamashush <38120991+yamashush@users.noreply.github.com>
2020-05-21 10:59:11 +0900

    :wrench: add git alias

--- .config/fish/conf.d/config.fish
+++ .config/fish/conf.d/config.fish
@@ -19,6 +19,28 @@ function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
 end
 
+#
+# alias git
+#
+alias g='git'
+alias gb='git branch'
+alias gs='git status'
+alias gd='git diff'
+alias gds='git diff --staged'
+alias ga.='git add .'
+alias gc='git commit'
+alias gcm='git commit -m'
+alias gcam='git commit -am'
+alias gp='git push'
+alias gpo='git push origin'
+alias gfo='git fetch origin'
+alias gfu='git fetch upstream'
+alias gsp='git stash pop'
+alias gch='git checkout'
+alias gchb='git checkout -b'
+alias gchm='git checkout master'
+alias gm='git merge'
+alias gmum='git merge upstream/master'
 
 #
 # auto ls
