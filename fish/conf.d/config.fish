4a039dc817766a2864e4573445536e6777566dad
yamashush <38120991+yamashush@users.noreply.github.com>
2022-07-31 11:52:24 +0900

    :fire: remove gomi

--- fish/conf.d/config.fish
+++ fish/conf.d/config.fish
@@ -1,61 +1,13 @@
-#
 # PATH
-#
-#set -x PATH $HOME/.anyenv/bin $PATH
-#set -Ux fish_user_paths $HOME/.anyenv/bin $fish_user_paths
 set -Ux fish_user_paths $HOME/.rbenv/bin $fish_user_paths
 set -x PATH $HOME/.rbenv/shims $PATH
-#set -x PATH $HOME/.rbenv/bin $PATH
-#set -x PATH $HOME/.nodenv/shims $PATH
-#set -x PATH $HOME/.nodenv/bin $PATH
-#set -x PATH $HOME/.pyenv/shims $PATH
-#set -x PATH /usr/local/opt/mysql@5.7/bin $PATH
 
-#set -x PATH /opt/homebrew/opt/openssl@3/bin: $PATH
-#set -gx LDFLAGS “-L/opt/homebrew/opt/openssl@3/lib”
-#set -gx CPPFLAGS “-I/opt/homebrew/opt/openssl@3/include”
-#set -gx PKG_CONFIG_PATH “/opt/homebrew/opt/openssl@3/lib/pkgconfig”
-
-#set -gx RUBY_CONFIGURE_OPTS "--with-openssl-dir=/opt/homebrew/opt/openssl@3/bin/openssl"
-#set -gx RUBY_CONFIGURE_OPTS "--with-openssl-dir=/usr/local/opt/openssl@1.1"
-
-
-#set -x PATH /opt/homebrew/opt/openssl@1.1/bin $PATH
-#set -gx LDFLAGS “-L/opt/homebrew/opt/openssl@1.1/lib”
-#set -gx CPPFLAGS “-I/opt/homebrew/opt/openssl@1.1/include”
+# Ruby
 set -gx RUBY_CONFIGURE_OPTS "--with-openssl-dir=/opt/homebrew/opt/openssl@1.1/"
-#set -gx RBENV_ROOT "~/.rbenv"
 set -gx RUBY_CFLAGS "-w"
-#set -gx LDFLAGS "-L/opt/local/lib"
-
-# rbenv init - fish | source
-#anyenv init - fish | source
-#eval (rbenv init - fish)
-
-#set -gx PATH '~/.rbenv/shims' $PATH
-#set -gx RBENV_SHELL fish
-
-#eval "$(rbenv init -)
-
-# https://docs.brew.sh/Shell-Completion
-# if test -d (brew --prefix)"/share/fish/completions"
-#     set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
-# end
-
-# if test -d (brew --prefix)"/share/fish/vendor_completions.d"
-#     set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
-# end
 
 # prompt
 # https://spacefish.matchai.dev/docs/Options.html
-
-# set -g fish_prompt_pwd_dir_length 0
-# set SPACEFISH_PROMPT_DEFAULT_SUFFIX 
-# set SPACEFISH_USER_SHOW always
-# set SPACEFISH_USER_SUFFIX " ᔦ๑° ꒳ °๑ᔨ "
-# set SPACEFISH_USER_SUFFIX " ᔦ๑°ω°๑ᔨ "
-# set SPACEFISH_USER_SUFFIX " (´・ω・`) "
-# set SPACEFISH_USER_SUFFIX " 🚀 "
 set SPACEFISH_TIME_SHOW true
 set SPACEFISH_DATE_SHOW true
 set SPACEFISH_TIME_PREFIX "⏰ "
@@ -85,32 +37,6 @@ end
 
 # fzf
 # https://github.com/jethrokuan/fzf 
-
-# bind \co '__fzf_open --editor'
-# bind -M insert "ç" fzf-cd-widget
-# set -U FZF_LEGACY_KEYBINDINGS 0
-# set -U FZF_DISABLE_KEYBINDINGS 0
-# set -U FZF_PREVIEW_FILE_CMD "head -n 30"
-# set -U FZF_PREVIEW_DIR_CMD "la"
-# 配下のファイルを絞り込みエディタで表示する
-# bind \cp "__fzf_open --editor --preview 'bat --style=numbers --color=always --line-range :500 {}'"
-# 履歴からコマンドを絞り込み実行する
-# bind \cr __fzf_reverse_isearch
-# alt+cで配下のディレクトを絞り込み移動する
-# bind \ec __fzf_cd
-
-# set -x FZF_DEFAULT_OPTS '
-#     --cycle
-#     --border
-#     --height=90%
-#     --layout=reverse 
-#     --marker="!"
-#     --color=fg:#bdae93,bg:#32302f,hl:#83a598
-#     --color=fg+:#ebdbb2,bg+:#3c3836,hl+:#83a598
-#     --color=info:#fabd2f,prompt:#fabd2f,pointer:#8ec07c
-#     --color=marker:#8ec07c,spinner:#8ec07c,header:#83a598
-# '
-
 set -x FZF_DEFAULT_OPTS '
     --border
 '
@@ -139,7 +65,6 @@ set -x FORGIT_FZF_DEFAULT_OPTS "
     --color=info:#fabd2f,prompt:#fabd2f,pointer:#8ec07c \
     --color=marker:#8ec07c,spinner:#8ec07c,header:#83a598
 "
-
 alias vv="fzf \
     --cycle \
     --height=90% \
@@ -174,9 +99,7 @@ alias gchm='git checkout main'
 alias gm='git merge'
 alias gmum='git merge upstream/main'
 
-#
 # auto ls
-#
 function cd
     if count $argv > /dev/null
         builtin cd "$argv"; and la
@@ -200,5 +123,5 @@ function fish_greeting
 end
 
 function load
-  exec fish
+    exec fish
 end
