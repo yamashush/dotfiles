93d3d35a90bbdace718ebf99a5668cfbd430fe97
yamashush <38120991+yamashush@users.noreply.github.com>
2022-08-15 10:10:05 +0900

    :hammer: add brew install script

---
+++ install_brew.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo "installing homebrew..."
+which brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "run brew doctor..."
+which brew >/dev/null 2>&1 && brew doctor
+
+echo "run brew update..."
+which brew >/dev/null 2>&1 && brew update
+
+echo "ok. run brew upgrade..."
+brew upgrade
+
+
+formulas=()
+
+echo "brew tap"
+# brew tap thirdparty
+brew tap homebrew/cask-fonts
+
+echo "brew install formula"
+for formula in "${formulas[@]}"; do
+    brew install $formula || brew upgrade $formula
+done
+
+# install gui up
+casks=()
+
+echo "brew casks"
+for cask in "${casks[@]}"; do
+    brew install --cask $cask
+done
+
+
+stores=()
+
+
+echo "app stores"
+for store in "${stores[@]}"; do
+    mas install $store
+done
+
+
+brew cleanup
+
+echo "brew installed"
