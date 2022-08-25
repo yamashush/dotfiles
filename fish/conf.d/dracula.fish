262fd4a0cf432437c2f9649a2f6ee47f523193f1
yamashush <38120991+yamashush@users.noreply.github.com>
2022-08-25 17:03:33 +0900

    :wrench: add doracula theme at fisher list

--- fish/conf.d/dracula.fish
+++ fish/conf.d/dracula.fish
@@ -1,25 +1,50 @@
+# Dracula Color Palette
 set -l foreground f8f8f2
-set -l selection  44475a
-set -l comment    6272a4
-
-set -l red    ff5555
+set -l selection 44475a
+set -l comment 6272a4
+set -l red ff5555
 set -l orange ffb86c
-set -l yellow f4f99d
-set -l green  50fa7b
-set -l cyan   8be9fd
-set -l pink   ff79c6
+set -l yellow f1fa8c
+set -l green 50fa7b
 set -l purple bd93f9
+set -l cyan 8be9fd
+set -l pink ff79c6
 
-set -g fish_color_autosuggestion $selection
-set -g fish_color_command        $cyan
-set -g fish_color_comment        $comment
-set -g fish_color_end            $orange
-set -g fish_color_error          $red
-set -g fish_color_escape         $pink
-set -g fish_color_normal         $foreground
-set -g fish_color_operator       $green
-set -g fish_color_param          $purple
-set -g fish_color_quote          $yellow
-set -g fish_color_redirection    $foreground
-set -g fish_color_search_match   --background=$selection
-set -g fish_color_selection      --background=$selection
+# Syntax Highlighting Colors
+set -gx fish_color_normal $foreground
+set -gx fish_color_command $cyan
+set -gx fish_color_keyword $pink
+set -gx fish_color_quote $yellow
+set -gx fish_color_redirection $foreground
+set -gx fish_color_end $orange
+set -gx fish_color_error $red
+set -gx fish_color_param $purple
+set -gx fish_color_comment $comment
+set -gx fish_color_selection --background=$selection
+set -gx fish_color_search_match --background=$selection
+set -gx fish_color_operator $green
+set -gx fish_color_escape $pink
+set -gx fish_color_autosuggestion $comment
+set -gx fish_color_cancel $red --reverse
+set -gx fish_color_option $orange
+
+# Default Prompt Colors
+set -gx fish_color_cwd $green
+set -gx fish_color_host $purple
+set -gx fish_color_host_remote $purple
+set -gx fish_color_user $cyan
+
+# Completion Pager Colors
+set -gx fish_pager_color_progress $comment
+set -gx fish_pager_color_background
+set -gx fish_pager_color_prefix $cyan
+set -gx fish_pager_color_completion $foreground
+set -gx fish_pager_color_description $comment
+set -gx fish_pager_color_selected_background --background=$selection
+set -gx fish_pager_color_selected_prefix $cyan
+set -gx fish_pager_color_selected_completion $foreground
+set -gx fish_pager_color_selected_description $comment
+set -gx fish_pager_color_secondary_background
+set -gx fish_pager_color_secondary_prefix $cyan
+set -gx fish_pager_color_secondary_completion $foreground
+set -gx fish_pager_color_secondary_description $comment
