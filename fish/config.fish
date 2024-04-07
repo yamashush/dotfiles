2a3b0a917d922895738d86dd16d8085a312d4eca
yamashush <38120991+yamashush@users.noreply.github.com>
2024-04-07 10:21:50 +0900

    🔥 Remove ***env init

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     eval (/opt/homebrew/bin/brew shellenv)
-    eval "$(nodenv init -)"
-    eval "$(pyenv init -)"
     status --is-interactive; and source (anyenv init -|psub)
 
     # prompt
