64edff24be80ab7590e916fb44d2cfc4adefb8fd
yamashush <38120991+yamashush@users.noreply.github.com>
2024-04-28 21:18:25 +0900

    🔧 Add keymap of filer & grep

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,6 +3,7 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+        vim.keymap.set('n', '<C-f>', builtin.find_files, {})
+        vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
     end
 }
