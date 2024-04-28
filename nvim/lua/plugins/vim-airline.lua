4fd9c944fc83aa359bd33144f9148a19f0b087d0
yamashush <38120991+yamashush@users.noreply.github.com>
2024-04-28 15:27:54 +0900

    🔧 Add nvim vim-airline-themes

--- nvim/lua/plugins/vim-airline.lua
+++ nvim/lua/plugins/vim-airline.lua
@@ -1,3 +1,7 @@
 return {
     "vim-airline/vim-airline",
+    init = function()
+        -- https://github.com/vim-airline/vim-airline/wiki/Screenshots
+        vim.g.airline_theme = 'violet'
+    end,
 }
