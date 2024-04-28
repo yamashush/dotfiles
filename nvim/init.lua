f2bff798b09b47b665701897aba52005dd5080d6
yamashush <38120991+yamashush@users.noreply.github.com>
2024-04-28 20:58:19 +0900

    🔧 Add vim catppuccin plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,19 @@
-
-
 -- Example using a list of specs with the default options
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
 
-require("core/lazy")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
 require('core/options')
+require("lazy").setup("plugins", opts)
