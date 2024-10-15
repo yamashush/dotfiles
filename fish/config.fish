414f9c845294af88cd937465b958d7fa1fe0c680
yamashu <yamashush@gmail.com>
2024-10-15 10:53:35 +0900

    Replace anyenv to nodenv

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     eval (/opt/homebrew/bin/brew shellenv)
-    status --is-interactive; and source (anyenv init -|psub)
+    status --is-interactive; and nodenv init - fish | source
 
     # prompt
     # https://github.com/starship/starship
